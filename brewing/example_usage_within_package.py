7ff5404a31676158eefb28285e7ef639b52e45ef
ASPP Student <student@localhost>
2022-09-07 11:24:41 +0200

    Play with imports

--- brewing/example_usage_within_package.py
+++ brewing/example_usage_within_package.py
@@ -1,1 +1,8 @@
 # Task: import make_example_potion from the module brew_potions.py
+#from brew_potions import make_example_potion
+import brew_potions as br
+
+
+#make_example_potion()
+br.make_example_potion("Julia")
+
